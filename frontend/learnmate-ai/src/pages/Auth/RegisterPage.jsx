afe6d4d4558e2ed3331127d73fbacc020d955c7f
dhadhalhardi2004 <dhadhalhardi2004@gmail.com>
2026-05-20 19:08:16 +0530

    Completed dashboard and authentication UI

--- frontend/learnmate-ai/src/pages/Auth/RegisterPage.jsx
+++ frontend/learnmate-ai/src/pages/Auth/RegisterPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-import authService from "../../service/authService";
+import authService from "../../services/authService";
 
 import {
   BrainCircuit,
